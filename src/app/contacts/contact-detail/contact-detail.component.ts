0d19c34e7ca1cf8f5bc645c80b12f352afe3b03a
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2023-04-26 01:37:50 -0600

    Added everything for week 2 assignment

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { Contact } from '../../contact.model';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent {
+  contact: Contact;
+
+  constructor() {
+    this.contact = new Contact(1, 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', null);
+  }
+}
