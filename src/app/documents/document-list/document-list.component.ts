508f89c63af42fc90163f64cd52617d20a4ced7a
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2023-05-09 17:41:39 -0600

    Added everything for lesson 4

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class DocumentListComponent {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(1, 'Document 1', 'This is document 1', 'document1url'),
+    new Document(2, 'Document 2', 'This is document 2', 'document2url'),
+    new Document(3, 'Document 3', 'This is document 3', 'document3url'),
+    new Document(4, 'Document 4', 'This is document 4', 'document4url'),
+    new Document(5, 'Document 5', 'This is document 5', 'documenturl')
+  ]
+  onDocumentSelected(document: Document) {
+  this.selectedDocumentEvent.emit(document);
+  }
 }
